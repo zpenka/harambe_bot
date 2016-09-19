855253d4625487136e702429eae322711633c718
Zack Penka & Daniel Berry <zpenka@shutterstock.com>
2016-09-19 19:08:40 -0400

    refactor

--- harambe.js
+++ harambe.js
@@ -1,35 +1,11 @@
 'use strict';
 
-const Botkit = require('botkit');
-const Cleverbot = require('cleverbot.io');
-
 const creds = {
   clever_api: {
     USER: process.env.CLEVER_API_USER,
     KEY: process.env.CLEVER_API_KEY,
   },
 };
-
-const clever_harambe = new Cleverbot(creds.clever_api.USER, creds.clever_api.KEY);
-
-clever_harambe.setNick('Harambe');
-
-clever_harambe.create((err, session) => {
-    if (err) {
-        console.log('Cleverbot create fail.');
-    } else {
-        console.log('Cleverbot create success.');
-    }
-});
-
-const controller = Botkit.slackbot({
-  debug: process.env.test || true,
-  log: process.env.test || true,
-});
-
-controller.spawn({
-  token: process.env.token,
-}).startRTM();
 
 const people = {
   Zack: 'U2BQVDSGL',
@@ -48,7 +24,35 @@ const ambience = [
   'ambient',
 ];
 
-const registerResponse = (listenFor, say, context) => controller.hears(listenFor, context, (bot, event) => bot.reply(event, say));
+const Botkit = require('botkit');
+const Cleverbot = require('cleverbot.io');
+
+const clever_harambe = new Cleverbot(
+  creds.clever_api.USER,
+  creds.clever_api.KEY
+);
+
+
+clever_harambe.create((err, session) => {
+    if (err) {
+        return console.log('Cleverbot create fail.');
+    }
+
+    clever_harambe.setNick('Harambe');
+
+    return console.log('Cleverbot create success.');
+});
+
+const harambe = Botkit.slackbot({
+  debug: process.env.test || true,
+  log: process.env.test || true,
+});
+
+harambe.spawn({
+  token: process.env.token,
+}).startRTM();
+
+const registerResponse = (listenFor, say, context) => harambe.hears(listenFor, context, (bot, event) => bot.reply(event, say));
 
 // Simple responses
 registerResponse('favorite band', 'Harambe and the Gone Apes, obviously', on_mention);
@@ -60,81 +64,45 @@ registerResponse('ape heaven', 'it fucking sucks you anus licking cunt', on_ment
 registerResponse('do you like', 'no, fuck that', on_mention);
 registerResponse('lit', ':party_parrot: :fast_parrot: :slow_parrot: :sassy_parrot: :slow_parrot: :goth_parrot: GONE APE MUTHAFUCKAAS :exploding_parrot: :moonwalking_parrot: :aussie_parrot: :deal_with_it_parrot: :aussie_conga_line_parrot:', on_mention);
 
-controller.hears('fuck you', on_mention, (harambe, event) => {
+// Slightly more complex responses TODO refactor this somehow
+harambe.hears('fuck you', on_mention, (bot, event) => {
   if (event.user === people.Ryan) {
-    return harambe.reply(event, 'god dammit Ryan you fucking chode, shut the fuck up');
+    return bot.reply(event, 'god dammit Ryan you fucking chode, shut the fuck up');
   }
 
-  return harambe.reply(event, 'fuck you too');
+  return bot.reply(event, 'fuck you too');
 });
 
-controller.hears('love you', on_mention, (harambe, event) => {
-  if (event.user === people.Tingle) {
-    return harambe.reply(event, 'no, just no. fuck no.');
-  }
-
-  return harambe.reply(event, ':heart:');
-});
-
-// Random Number Generator
-controller.hears('rng', on_mention, (harambe, event) => {
+harambe.hears('rng', on_mention, (bot, event) => {
   const random_number = Math.floor(Math.random() * (999 - 1 + 1)) + 1;
 
-  return harambe.reply(event, `Here is a random number between 1 and 999 you fucking shit: ${random_number}`);
+  return bot.reply(event, `Here is a random number between 1 and 999 you fucking shit: ${random_number}`);
 });
 
-// He still doesn't like Tingle
-controller.hears('do you like me', on_mention, (harambe, event) => {
+harambe.hears('do you like me', on_mention, (bot, event) => {
   const person = event.user;
   const answer = person === people.Tingle ? 'no' : 'yes';
 
-  return harambe.reply(event, answer);
+  return bot.reply(event, answer);
 });
 
+harambe.hears('love you', on_mention, (bot, event) => {
+  if (event.user === people.Tingle) {
+    return bot.reply(event, 'no, just no. fuck no.');
+  }
 
-// Welcome new users
-controller.on('user_channel_join', (harambe, event) => {
+  return bot.reply(event, ':heart:');
+});
+
+harambe.on('user_channel_join', (bot, event) => {
   const channel = event.channel;
   const name = event.user_profile.first_name;
   const text = `Welcome to the forums, ${name}, my name is bosk1, fuck you`;
 
-  return harambe.say({ text, channel });
+  return bot.say({ text, channel });
 });
 
-// Eloquence conversation
-controller.hears(['be eloquent'], on_mention, (harambe, event) => {
-    const first = (err, convo) => {
-      convo.ask('How eloquent shall I be?', (response, convo) => {
-        convo.say('Sounds good. Even though I am an ape, I can try my best.');
-        second(response, convo);
-        convo.next();
-      });
-    };
-
-    const second = (response, convo) => {
-      convo.ask('How eloquent are you?', (response, convo) => {
-        convo.say('Ok.')
-        third(response, convo);
-        convo.next();
-      });
-    };
-
-    const third = (response, convo) => {
-      convo.ask('I think we can be great, eloquent friends. Do you agree?', (response, convo) => {
-        convo.say('Ok! Good bye, shit eater.');
-        convo.next();
-      });
-    };
-
-    if (event.user === people.Tingle) {
-      return harambe.reply(event, 'no, fuck you');
-    }
-
-    return harambe.startConversation(event, first);
-});
-
-// greeting conversation
-controller.hears(['sup', 'hey', 'hello'], on_mention, (harambe, event) => {
+harambe.hears(['sup'], on_mention, (bot, event) => {
     const greeting = (response, convo) => {
       convo.ask('nm, u? LOL :neverusethis:', (response, convo) => {
         convo.say('shut the fuck up');
@@ -142,18 +110,19 @@ controller.hears(['sup', 'hey', 'hello'], on_mention, (harambe, event) => {
       });
     };
 
-    harambe.startConversation(event, greeting);
+    bot.startConversation(event, greeting);
 });
 
-// Set up clever bot
-controller.hears('', ambience, (harambe, event) => {
+// Set up clever bot NOTE: These are last for a reason, so they do not override the responses defined above
+// TODO: Refactor these two
+harambe.hears('', ambience, (bot, event) => {
   const message = event.text;
   const random_number = Math.floor(Math.random() * 99);
 
   if (random_number === 0) {
     return clever_harambe.ask(message, (err, response) => {
         if (!err) {
-            return harambe.reply(event, response);
+            return bot.reply(event, response);
         } else {
             console.log(`Cleverbot err: ${err}`);
         }
@@ -163,7 +132,7 @@ controller.hears('', ambience, (harambe, event) => {
   return;
 });
 
-controller.hears('', on_mention, (harambe, event) => {
+harambe.hears('', on_mention, (bot, event) => {
   const message = event.text;
 
   return clever_harambe.ask(message, (err, response) => {
@@ -172,6 +141,6 @@ controller.hears('', on_mention, (harambe, event) => {
       return;
     }
 
-    return harambe.reply(event, response);
+    return bot.reply(event, response);
   });
 });
